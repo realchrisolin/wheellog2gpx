56819115813bc3425f32e7a93d400e4fb11f1505
Chris Olin <contact@chrisolin.com>
2022-08-26 16:57:35 -0400

    fix all the things

--- wheellog2gpx.py
+++ wheellog2gpx.py
@@ -9,14 +9,11 @@ import time
 def csv2gpx(file, verbose=False, debug=False):
     # This function is essentially a wrapper for gpxpy, which is really
     # creating the GPX file under the hood.
-    test = os.path.normpath(file)
     csv_data = open(file, 'r')
     csv_reader = csv.reader(csv_data)
     gpx_data = gpxpy.gpx.GPX()
     gpx_file = file[:-3] + 'gpx' # export to the same directory/filename as .csv but with .gpx extension
     reader_index = 0
-    tz = time.tzname
-    data_points = []
     wrapper = {}
     headers = next(csv_reader, None)
     print(headers)
@@ -39,33 +36,38 @@ def csv2gpx(file, verbose=False, debug=False):
     gpx_track.segments.append(gpx_segment)
 
     for row in csv_reader:
-        reader_index = + 1
+        reader_index += 1
+        if verbose is True:
+            print(f'Processing line {reader_index}')
         for id, data in zip(headers, row):
             wrapper[id] = data
         # TODO: finish sanitizing date/time and pass it to gpxpy
-        sanitized_time = datetime.strptime(f"{wrapper['date']} {wrapper['time']}", '%m/%d/%Y %H:%M.%S')
+        sanitized_time = datetime.strptime(f"{wrapper['date']} {wrapper['time']}", '%Y-%m-%d %H:%M:%S.%f')
         gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(latitude=wrapper['latitude'], longitude=wrapper['longitude'],
                                                           elevation=wrapper['gps_alt'], time=sanitized_time,
                                                           speed=wrapper['speed']))
-
+        if debug is True:
+            print(f'{wrapper}')
+            print('-'*10)
     try:
         gpx_writer = open(gpx_file, 'x')
     except FileExistsError:
         gpx_writer = open(gpx_file, 'w')
-    gpx_writer(gpx_data.to_xml(version='1.1'))
+    gpx_writer.write(gpx_data.to_xml(version='1.1'))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="WheelLog2GPX")
     parser.add_argument("csv_file")
+    parser.add_argument('-v', '--verbose', help="additional console logging", action="store_true")
+    parser.add_argument('-d', '--debug', help="print each line from input CSV", action="store_true")
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
-    csv2gpx(args.csv_file)
-
+    csv2gpx(args.csv_file, verbose=args.verbose, debug=args.debug)
 
 if __name__ == '__main__':
     main()
